c1819df87fbbb6ee8b26261c908c0879d5889bd9
Christos Marroquin <christos.marroquin@hotmail.com>
2023-04-24 23:26:31 -0600

    fadeIn

--- src/pages/auth/AuthPage.jsx
+++ src/pages/auth/AuthPage.jsx
@@ -3,7 +3,7 @@ import LoginForm from "../../components/forms/login/LoginForm";
 
 const AuthPage = () => {
   return (
-    <div className="fullHeight d-flex align-items-center justify-content-center">
+    <div className="fadeIn fullHeight d-flex align-items-center justify-content-center">
       <LoginForm />
     </div>
   );
